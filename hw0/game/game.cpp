8ec56908c5e1169fbdda9eab13aaa91cac01602c
gabe pier <gabepier12@gmail.com>
2021-01-22 21:29:13 -0900

    Add game description to hw0

---
+++ hw0/game/game.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Set in a brutally unforgiving post-apocalyptic world overrun by the undead, 7 Days to Die is an open-world game\n";
+	std::cout << "that is a unique combination of first person shooter, survival horror, tower defense, and role-playing games.";
+		return 0;
+}
---
+++ hw0/poem/poem.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Once upon a midnight dreary, while I pondered, weak and weary,\n";
+    std::cout << "Over many a quaint and curious volume of forgotten lore—\n";
+    std::cout << "    While I nodded, nearly napping, suddenly there came a tapping,\n";
+    std::cout << "As of some one gently rapping, rapping at my chamber door.\n";
+    std::cout << "Tis some visitor, I muttered, tapping at my chamber door\n";
+    std::cout << "            Only this and nothing more.";
+    return 0;
+}

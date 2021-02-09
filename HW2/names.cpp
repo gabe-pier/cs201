4841fd0758acff7890b8ff621e74c8021ffa1369
gabe pier <gabepier12@gmail.com>
2021-02-08 22:19:50 -0900

    Added a function that returns the lengths of the vector components.

--- HW2/names.cpp
+++ HW2/names.cpp
@@ -15,31 +15,61 @@ using std::string;
 using std::cout;
 using std::getline;
 using std::cin;
-
+using std::endl;
 
 /*
 * Function asks the user for an input of 10 names.
 */
-void InputNames()
+void InputNames(vector<string> &names)
 {
-	vector<string> names;
 	for (int i = 0; i < 10; i++) {
 		string name;
 		cout << "Please enter a name: ";
 		getline(cin, name);
 		names.push_back(name);
-	}
+	};
 }
 /*
 * Searches the list for a specific name.
 */
 bool DoesNameExist(const string& nameToFind, const vector<string>& names)
 {
-	if 
+	if (std::find(names.begin(), names.end(), "nameToFind") != names.end())
+		return true;
+	else
+		return false;
+}
+
+/*
+* A function that prints the names in the vector.
+*/
+void PrintNames(const vector<string>& names)
+{
+	cout << "The vector elements are : ";
+
+	for (int i = 0; i < names.size(); i++)
+		cout << names.at(i) << ' ';
+}
+/*
+* A funtion that prints the lengths of the names.
+*/
+void NameLength(const vector<string>& names)
+{
+	cout << "\nThe respective lengths are: ";
+
+	for (int i = 0; i < names.size(); i++)
+		cout << size(names.at(i)) << ' ';
 }
 
 int main(int argc, char **argv) 
 {
-	InputNames();
+	vector<string> names;
+	InputNames(names);
+	string namesToFind;
+	cout << "Type a name to search for: ";
+	cin >> namesToFind;
+	DoesNameExist(namesToFind, names);
+	PrintNames(names);
+	NameLength(names);
 }
 

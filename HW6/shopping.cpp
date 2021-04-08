ef5a054331cb112cf58ed021712adb97a0fd792e
gabe pier <gabepier12@gmail.com>
2021-04-07 21:52:11 -0800

    Created shopping(), initialized struct, added shopping() into shopping.hpp.

---
+++ HW6/shopping.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+
+void shopping()
+{
+	struct Record {
+		double unitPrice;
+		int units;
+	} soda, apple, raspberries, bread, eggs; 
+
+}
--- HW6/shopping.hpp
+++ HW6/shopping.hpp
@@ -1,1 +1,7 @@
 #pragma once
+#ifndef shopping_hpp
+#define shopping_hpp
+using namespace std;
+
+void shopping();
+#endif

ce0ab7ad1aeca83dc8bf4e386256019f91fdb60e
gabe pier <gabepier12@gmail.com>
2021-04-07 21:37:21 -0800

    Finished implementing RandomBetweenU(), RandomBetweenN(), RandomBetween(), and PrintDistribution() in main.

--- HW6/random-map.cpp
+++ HW6/random-map.cpp
@@ -6,25 +6,41 @@
 #include <cmath>
 #include <stdlib.h>
 
-
-int RandomBetweenU(int first, int last, int seed)
+// Uniform Dist function
+int RandomBetweenU(int first, int last)
 {
+	std::random_device r;
+	std::default_random_engine e1(r());
 	std::uniform_int_distribution<int> uniform_dist(first, last);
-	int Uvalue = uniform_dist(seed);
+	int Uvalue = uniform_dist(e1);
 	return Uvalue;
 }
 
-int RandomBetweenN(int first, int last, int seed)
+//Normal Dist function
+int RandomBetweenN(int first, int last)
 {
-	std::normal_distribution<int> normal_dist(first, last);
-	int Nvalue = normal_dist(seed);
+	std::random_device r;
+	std::default_random_engine e1(r());
+	double mean = (first + last) / 2;
+	std::normal_distribution<double> normal_dist(mean, 1);
+	int Nvalue = normal_dist(e1);
 	return Nvalue;
 }
 
+// C++ rand function
 int RandomBetween(int first, int last)
 {
 	int Rvalue = rand() % last + first;
 	return Rvalue;
+}
+
+// Histogram Plot
+void PrintDistribution(const std::map<int, int>& hist)
+{
+	for (auto p : hist) {
+		std::cout << std::fixed << std::setprecision(1) << std::setw(2)
+			<< p.first << ' ' << std::string(p.second / 200, '*') << '\n';
+	}
 }
 
 int main()
@@ -33,27 +49,36 @@ int main()
 	std::random_device r;
 	// Choose a random mean between 1 and 6
 	std::default_random_engine e1(r());
-	int f;
-	int l;
+
+	// Calling input from the user to set bounds of the generators.
+	double f;
+	double l;
 
 	std::cout << "please input a lower bound: ";
 	std::cin >> f;
 	std::cout << "\nplease input an upper bound: ";
 	std::cin >> l;
-	std::uniform_int_distribution<int> uniform_dist(f, l);
-	int mean = uniform_dist(e1);
-	std::cout << "Randomly-chosen mean: " << mean << '\n';
-	// Generate a normal distribution around that mean
-	std::seed_seq seed2{ r(), r(), r(), r(), r(), r(), r(), r() };
-	std::mt19937 e2(seed2);
-	std::normal_distribution<> normal_dist(mean, 2);
-	std::map<int, int> hist;
+
+	// Calling random numbers with bounds
+	RandomBetweenN(f, l);
+	RandomBetweenU(f, l);
+	RandomBetween(f, l);
+
+	// Generating Values
+	std::map<int, int> histN;
+	std::map<int, int> histU;
+	std::map<int, int> histR;
 	for (int n = 0; n < 10000; ++n) {
-		++hist[std::round(normal_dist(e2))];
+		++histN[std::round(RandomBetweenN(f, l))];
+		++histU[std::round(RandomBetweenU(f, l))];
+		++histR[std::round(RandomBetween(f, l))];
 	}
-	std::cout << "Normal distribution around " << mean << ":\n";
-	for (auto p : hist) {
-		std::cout << std::fixed << std::setprecision(1) << std::setw(2)
-			<< p.first << ' ' << std::string(p.second / 200, '*') << '\n';
-	}
+
+	// Calling function to pass to the histogram function
+	std::cout << "normal distribution" << std::endl;
+	PrintDistribution(histN);
+	std::cout << std::endl << "uniform distribution" << std::endl;
+	PrintDistribution(histU);
+	std::cout << std::endl << "rand distribution" << std::endl;
+	PrintDistribution(histR);
 }

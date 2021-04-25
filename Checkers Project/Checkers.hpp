d23f02fc94377362a260f1cf29e47deb45c21d60
gabe pier <gabepier12@gmail.com>
2021-04-25 05:49:49 -0800

    initialized functions in Checkers.hpp to get piece coordinates on the board.

--- Checkers Project/Checkers.hpp
+++ Checkers Project/Checkers.hpp
@@ -8,12 +8,36 @@
 class Checkers
 {
 public:
-	void initBoard();
+	//board and piece initialization
+	void initBoard(); 
 	void printBoard();
 	void initPieces();
 
+	//setting up functions to prompt the user for cordinate input
+	void setx(int x) {
+		_xcord = x;
+	}
+	int getx() {
+		return _xcord;
+	}
+	void sety(int y) {
+		_ycord = y;
+	}
+	int gety() {
+		return _ycord;
+	}
+
+	//function to move pieces
+	void movepiece();
+
+
 private:
+	//board coordinates and player pieces
 	char board[8][8];
 	char p1 = 'X';
 	char p2 = 'O';
+
+	//piece coordinate values
+	int _xcord;
+	int _ycord;
 };
